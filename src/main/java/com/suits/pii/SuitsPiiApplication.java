1906a5ed0ada8244b7023506c9262f6612de5723
sblashuk <sblashuk@logopak.de>
2020-04-23 14:55:12 +0200

    init

---
+++ src/main/java/com/suits/pii/SuitsPiiApplication.java
@@ -0,0 +1,13 @@
+package com.suits.pii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuitsPiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuitsPiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suits/pii/SuitsPiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suits.pii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuitsPiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
